877e815b933a2090922e6fee1b160846989d7f2d
Zoe Turner <zoe.turner2@nottshc.nhs.uk>
2021-07-10 19:13:13 +0100

    Added function to calculate the percentage of year of equal pay ending and added male mean and median.

--- app.R
+++ app.R
@@ -16,27 +16,50 @@ library(shinyWidgets)
 # url <- "https://gender-pay-gap.service.gov.uk/viewing/download-data/2020"
 # download.file(url, dest = "pay-gap-202021.csv")
 
+# Also recode greater than -100% to 100% for conversion to date percentages
+
 raw <- read_csv(here::here("pay-gap-202021.csv")) %>% 
     janitor::clean_names() %>% 
     select(employer_name,
            diff_mean_hourly_percent,
-           diff_median_hourly_percent
-    )
+           diff_median_hourly_percent) %>% 
+    mutate(diff_mean_hourly_percent = case_when(diff_mean_hourly_percent < -100 ~ -100,
+                                                TRUE ~ diff_mean_hourly_percent),
+           diff_median_hourly_percent = case_when(diff_median_hourly_percent < -100 ~ -100,
+                                                TRUE ~ diff_median_hourly_percent)
+           )
 
+equal_pay_date <- function(name, gender){
+    
+    avg_name <- paste0("diff_", name, "_hourly_percent")
+    
+    average_df <- raw %>%
+        mutate(year = as.Date('2020-01-01'),
+               yday = year + (round(365 - round(abs(.data[[avg_name]])/ 100 * 365)) - 1),
+               avg_day   = lubridate::day(yday),
+               avg_month = lubridate::month(yday, label = TRUE, abbr = FALSE)
+        ) %>% 
+        select(employer_name,
+               !!paste0(gender, "_", name, "_test") := yday,
+               !!paste0(gender, "_", name, "_day") := avg_day,
+               !!paste0(gender, "_", name, "_month") := avg_month,
+               diff_mean_hourly_percent,
+               diff_median_hourly_percent
+               
+        )
+    
+        return(average_df)
+}
 
-convert <- raw %>% 
-    mutate(year = as.Date('2020-01-01'),
-           median_year_per = round(diff_median_hourly_percent / 100 * 365),
-           median_year_not_paid = round(365 - median_year_per),
-           median_yday = year + (median_year_not_paid - 1),
-           median_day   = lubridate::day(median_yday),
-           median_month = lubridate::month(median_yday, label = TRUE, abbr = FALSE),
-           mean_year_per = round(diff_mean_hourly_percent / 100 * 365),
-           mean_year_not_paid = round(365 - mean_year_per),
-           mean_yday = year + (mean_year_not_paid - 1),
-           mean_day   = lubridate::day(mean_yday),
-           mean_month = lubridate::month(mean_yday, label = TRUE, abbr = FALSE)
-    )
+female_median <- equal_pay_date("median", "female")
+female_mean <- equal_pay_date("mean", "female")
+male_median <- equal_pay_date("median", "male")
+male_mean <- equal_pay_date("mean", "male")
+
+convert <- female_mean %>% 
+    left_join(female_median) %>% 
+    left_join(male_mean) %>% 
+    left_join(male_median)
 
 orgs <- convert %>% 
     select(employer_name) %>% 
@@ -74,20 +97,34 @@ server <- function(input, output, session) {
     
     output$sentence <- renderText({ 
         org_name() %>% 
-            mutate(formatting = case_when(input$average == "Median" & diff_median_hourly_percent > 0 ~
+            mutate(formatting = case_when(input$average == "Median" & 
+                                              diff_median_hourly_percent > 0 ~
                                               paste0("effectively stops paying women on ",
-                                                     median_day, " ", median_month, ", a pay gap of ",
-                                                     diff_median_hourly_percent, "%"),
-                                          input$average == "Median" & diff_median_hourly_percent <= 0 ~ 
-                                              paste0("pays women for the full 12 months, women outearn men by ",
-                                                        abs(diff_median_hourly_percent), "%"),
-                                          input$average == "Mean" & diff_mean_hourly_percent > 0 ~
+                                                     female_median_day, " ", 
+                                                     female_median_month, ", a pay gap of ",
+                                                     diff_median_hourly_percent, 
+                                                     "%"),
+                                          input$average == "Median" & 
+                                              diff_median_hourly_percent <= 0 ~ 
+                                              paste0("pays women for the full 12 months, and effectively stops paying men on ",
+                                                     male_median_day, " ",
+                                                     male_median_month, ", a pay gap of ",
+                                                     abs(diff_median_hourly_percent), 
+                                                     "%"),
+                                          input$average == "Mean" & 
+                                              diff_mean_hourly_percent > 0 ~
                                               paste0("effectively stops paying women on ",
-                                                     mean_day, " ", mean_month, ", a pay gap of ",
-                                                     diff_mean_hourly_percent, "%"),
+                                                     female_mean_day, " ", 
+                                                     female_mean_month, 
+                                                     ", a pay gap of ",
+                                                     diff_mean_hourly_percent, 
+                                                     "%"),
                                           input$average == "Mean" & diff_mean_hourly_percent <= 0 ~ 
-                                              paste0("pays women for the full 12 months, women outearn men by ",
-                                                     abs(diff_mean_hourly_percent), "%"),
+                                              paste0("pays women for the full 12 months, and effectively stops paying men on ",
+                                                     male_mean_day, " ",
+                                                     male_mean_month, ", a pay gap of ",
+                                                     abs(diff_mean_hourly_percent), 
+                                                     "%"),
                                           TRUE ~ NA_character_)) %>% 
             pull(formatting)
     })
